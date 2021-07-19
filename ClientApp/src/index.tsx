5c363b11f71698c7f1ff8bfa822383ef68772ce6
DESKTOP-9L2KC96\jason <jasonrawlins@hotmail.com>
2021-07-19 15:21:36 -0600

    Got Phaser to render with React

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -6,8 +6,10 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Phaser from "phaser";
+import PlayGame from "./components/phaser/Phaser";
+import App from './App';
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
@@ -15,6 +17,16 @@ const history = createBrowserHistory({ basename: baseUrl });
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const store = configureStore(history);
+
+export const config = {
+    type: Phaser.AUTO,
+    parent: "phaser",
+    width: 800,
+    height: 600,
+    scene: PlayGame
+};
+
+const game = new Phaser.Game(config);
 
 ReactDOM.render(
     <Provider store={store}>
