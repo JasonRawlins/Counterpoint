5da5a02f90682687434393ab6c9ebc4ffab26077
DESKTOP-IRR283R\jason <jasonrawlins@hotmail.com>
2021-07-26 13:00:15 -0600

    The grand staff lines are now displaying.

--- ClientApp/src/phaser/game.ts
+++ ClientApp/src/phaser/game.ts
@@ -20,21 +20,53 @@ export class GameScene extends Phaser.Scene {
     }
 
     public create() {
-        const wholeNoteImage = this.add.image(0, 0, "musical-symbols", "whole-note.png").setOrigin(0);
-        wholeNoteImage.setInteractive();
-        wholeNoteImage.setData("name", "Whole note");
-        this.input.on("pointerover", (event: string | symbol, gameObject: Phaser.GameObjects.Image[]) => {
-            feedbackText.text = gameObject[0].getData("name");
+        //const wholeNoteImage = this.add.image(0, 0, "musical-symbols", "whole-note.png").setOrigin(0);
+        //wholeNoteImage.setInteractive();
+        //wholeNoteImage.setData("name", "Whole note");
+
+        this.createGrandStaff();
+
+        
+
+        this.input.on("pointerover", (event: string | symbol, gameObjects: Phaser.GameObjects.GameObject[]) => {
+            feedbackText.text = gameObjects[0].name;
         });
 
-        const eLine = this.add.line(0, 0, 0, 50, 300, 50, 0x000000);
-
-        feedbackText = this.add.text(0, 250, "Feedback");
+        feedbackText = this.add.text(0, 250, "Feedback", { color: "#000000" });
         //this.add.image(0, 20, "musical-symbols", "treble-clef.png").setOrigin(0);
         //this.add.image(0, 120, "musical-symbols", "bass-clef.png").setOrigin(0);
     }
 
     public update() {
+    }
+
+    createGrandStaff() {
+        // Treble staff
+        const f5Line = this.createLine("f5 line", 40);
+        const d5Line = this.createLine("d5 line", 50);
+        const b4Line = this.createLine("b4 line", 60);
+        const g4Line = this.createLine("g4 line", 70);
+        const e4Line = this.createLine("e4 line", 80);
+
+        // Bass staff
+        const a3Line = this.createLine("a3 line", 120);
+        const f3Line = this.createLine("f3 line", 130);
+        const d3Line = this.createLine("d3 line", 140);
+        const b2Line = this.createLine("b2 line", 150);
+        const g2Line = this.createLine("g2 line", 160);
+
+
+
+    }
+
+    createLine(name: string, y: number): Phaser.GameObjects.Line {
+        const line = this.add.line(0, y, 0, 0, 300, 0, 0x000000).setOrigin(0);
+        line.name = name;
+        line.setInteractive();
+        //eLine.setLineWidth(5, 5);
+
+        return line;
+
     }
 
     public onObjectClicked(event: string | symbol, gameObject: Phaser.GameObjects.Image) {
