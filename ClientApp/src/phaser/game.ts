23696d8a6f9afa42f705126822d9785e15d0759e
DESKTOP-IRR283R\jason <jasonrawlins@hotmail.com>
2021-07-26 13:57:49 -0600

    Treble staff and symbol are now displayed. The height of the whole note image is used as the base unit of measurement.

--- ClientApp/src/phaser/game.ts
+++ ClientApp/src/phaser/game.ts
@@ -1,5 +1,11 @@
 import Phaser from "phaser";
 
+const screenSize = { width: 675, height: 200 };
+// These are the w x h dimensions of the whole note image, which will be used 
+// to calculate all other measurements as a percentage of the whole note width.
+const wholeNoteImageHieght = 12;
+const unit = (wholeNoteImageHieght / screenSize.height) * 100;
+
 let feedbackText: Phaser.GameObjects.Text;
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
@@ -20,9 +26,9 @@ export class GameScene extends Phaser.Scene {
     }
 
     public create() {
-        //const wholeNoteImage = this.add.image(0, 0, "musical-symbols", "whole-note.png").setOrigin(0);
-        //wholeNoteImage.setInteractive();
-        //wholeNoteImage.setData("name", "Whole note");
+        const wholeNoteImage = this.add.image(0, 0, "musical-symbols", "whole-note.png").setOrigin(0);
+        wholeNoteImage.name = "Whole note";
+        wholeNoteImage.setInteractive();
 
         this.createGrandStaff();
 
@@ -33,8 +39,6 @@ export class GameScene extends Phaser.Scene {
         });
 
         feedbackText = this.add.text(0, 250, "Feedback", { color: "#000000" });
-        //this.add.image(0, 20, "musical-symbols", "treble-clef.png").setOrigin(0);
-        //this.add.image(0, 120, "musical-symbols", "bass-clef.png").setOrigin(0);
     }
 
     public update() {
@@ -42,31 +46,34 @@ export class GameScene extends Phaser.Scene {
 
     createGrandStaff() {
         // Treble staff
-        const f5Line = this.createLine("f5 line", 40);
-        const d5Line = this.createLine("d5 line", 50);
-        const b4Line = this.createLine("b4 line", 60);
-        const g4Line = this.createLine("g4 line", 70);
-        const e4Line = this.createLine("e4 line", 80);
+        const topOfStaff = unit * 5;
+        const f5Line = this.createLine("f5 line", topOfStaff + unit * 2);
+        const d5Line = this.createLine("d5 line", topOfStaff + unit * 4);
+        const b4Line = this.createLine("b4 line", topOfStaff + unit * 6);
+        const g4Line = this.createLine("g4 line", topOfStaff + unit * 8);
+        const e4Line = this.createLine("e4 line", topOfStaff + unit * 10);
+        
+        var trebleClef = this.add.image(unit * 3, unit, "musical-symbols", "treble-clef.png").setOrigin(0);
+        trebleClef.setScale(1.32);
+
 
         // Bass staff
-        const a3Line = this.createLine("a3 line", 120);
-        const f3Line = this.createLine("f3 line", 130);
-        const d3Line = this.createLine("d3 line", 140);
-        const b2Line = this.createLine("b2 line", 150);
-        const g2Line = this.createLine("g2 line", 160);
-
-
+        //const a3Line = this.createLine("a3 line", 120);
+        //const f3Line = this.createLine("f3 line", 130);
+        //const d3Line = this.createLine("d3 line", 140);
+        //const b2Line = this.createLine("b2 line", 150);
+        //const g2Line = this.createLine("g2 line", 160);
+        //this.add.image(13, 120, "musical-symbols", "bass-clef.png").setOrigin(0);
 
     }
 
-    createLine(name: string, y: number): Phaser.GameObjects.Line {
-        const line = this.add.line(0, y, 0, 0, 300, 0, 0x000000).setOrigin(0);
+    createLine(name: string, y: number) {
+        const line = this.add.line(10, y, 0, 0, 300, 0, 0x000000).setOrigin(0);
         line.name = name;
         line.setInteractive();
-        //eLine.setLineWidth(5, 5);
+        line.setLineWidth(1);
 
         return line;
-
     }
 
     public onObjectClicked(event: string | symbol, gameObject: Phaser.GameObjects.Image) {
@@ -78,8 +85,8 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     title: "Counterpoint",
     type: Phaser.AUTO,
     scale: {
-        width: 400,
-        height: 300
+        width: screenSize.width,
+        height: screenSize.height
     },
     physics: {
         default: "arcade",
