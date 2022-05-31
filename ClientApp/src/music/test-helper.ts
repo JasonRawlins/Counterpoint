b45a866494783558cd642619f4c46a956339d0ad
LAPTOP\jason <jasonrawlins@hotmail.com>
2022-05-31 17:42:22 -0600

    Removing code because I can't get it deployed to aws. I'll reestablish it soon.

--- ClientApp/src/music/test-helper.ts
+++
@@ -1,9 +0,0 @@
-import { Clef, Key } from "./core";
-import { Exercise, Voice, VoicePosition } from "./counterpoint";
-
-export function createExercise(cantusFirmusNotes: string, counterpointNotes: string) {
-    return new Exercise(
-        Key.c,
-        new Voice(VoicePosition.bottom, Clef.bass, cantusFirmusNotes, true),
-        new Voice(VoicePosition.top, Clef.treble, counterpointNotes));
-}
--- Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Counterpoint.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-
-    }
-}
